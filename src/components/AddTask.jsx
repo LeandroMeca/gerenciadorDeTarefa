3f0b4fd907a09245b98b8e394db4ad3442804a2b
LeandroMeca <leandromeca20@outlook.com>
2025-11-17 15:07:37 -0300

    up first version

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Input from "./Input";
+
+function AddTask({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <div className="spacy-4 p-6 bg-slate-200 rounded-md shadow flex flex-col gap-2">
+      <Input
+        type="text"
+        placeholder="Digite o titulo da tarefa"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <Input
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button
+        onClick={() => {
+          if (!title.trim() || !description.trim()) {
+            return alert("Por favor, preencha todos os campos da tarefa.");
+          }
+
+          onAddTaskSubmit(title, description);
+          setTitle("");
+          setDescription("");
+        }}
+        className="bg-slate-500 text-white px-4 py-2 rouded-md font-medium"
+      >
+        Adicionar
+      </button>
+    </div>
+  );
+}
+
+export default AddTask;
